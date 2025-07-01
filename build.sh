ddd6e7b74ff77eb892973a7391fc7f34025e6618
Michał Nowacki <michal.nowacki@reef.pl>
2025-07-01 16:56:31 +0200

    build linux images

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ ORG_NAME=${ORG_NAME:-"backend-developers-ltd"}
 PROJECT_NAME=${PROJECT_NAME:-"self-updater"}
 ENV_NAME=${ENV_NAME:-"prod"}
 
-# Build the Docker image with build arguments
+# Build the Docker image with build arguments for x86 Linux platform
 docker build \
+  --platform linux/amd64 \
   --build-arg ORG_NAME="$ORG_NAME" \
   --build-arg PROJECT_NAME="$PROJECT_NAME" \
   -t backenddevelopersltd/luxor-updater-${ENV_NAME}:latest \
@@ -16,3 +17,4 @@ docker build \
 echo "Built image: backenddevelopersltd/luxor-updater-${ENV_NAME}:latest"
 echo "With GitHub repo: $ORG_NAME/$PROJECT_NAME"
 echo "Branch: deploy-compose-${ENV_NAME}"
+echo "Platform: linux/amd64 (x86 Linux)"
